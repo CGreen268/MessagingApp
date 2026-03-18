78e4803fb4b54e2fe770062e572d55488340ae4c
JBlack148 <2683314@dundee.ac.uk>
2026-03-18 11:19:30 +0000

    feat: basic swing window

--- messaging_application/src/main/java/io/github/ehteam/messaging/Main.java
+++ messaging_application/src/main/java/io/github/ehteam/messaging/Main.java
@@ -1,7 +1,16 @@
 package io.github.ehteam.messaging;
 
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Messaging App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+        JList<String> chats = new JList<>();
     }
 }
