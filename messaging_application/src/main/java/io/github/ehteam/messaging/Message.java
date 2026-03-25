d39d6de4425d7c0f438c1f3538f173fdcc5beb1a
JBlack148 <2683314@dundee.ac.uk>
2026-03-25 12:36:05 +0000

    feat: adding time and read to messages class

--- messaging_application/src/main/java/io/github/ehteam/messaging/Message.java
+++ messaging_application/src/main/java/io/github/ehteam/messaging/Message.java
@@ -4,10 +4,14 @@ public class Message {
 
     private String sender;
     private String content;
+    private boolean read;
+    private String time;
 
-    public Message(String sender, String content) {
+    public Message(String sender, String content, boolean read, String time) {
         this.sender = sender;
         this.content = content;
+        this.read = read;
+        this.time = time;
     }
 
     public String sender() {
@@ -16,5 +20,13 @@ public class Message {
 
     public String text() {
         return content;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public String time() {
+        return time;
     }
 }
